1fd1a5774dc2e85590c960fde0d2bb5d7317c269
walkiger <116552530+walkiger@users.noreply.github.com>
2025-01-20 17:18:12 +0100

    added functionality to update blog posts

--- app.py
+++ app.py
@@ -48,6 +48,31 @@ def add():
 
     return render_template('add.html')
 
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    # Load existing posts
+    blog_posts = load_posts()
+
+    # Find the blog post with the given ID
+    post = next((post for post in blog_posts if post['id'] == post_id), None)
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        # Update the post with form data
+        post['author'] = request.form.get('author')
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+
+        # Save updated posts to JSON file
+        save_posts(blog_posts)
+
+        # Redirect to the home page
+        return redirect(url_for('index'))
+
+    # Else, it's a GET request
+    return render_template('update.html', post=post)
+
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
     # Load existing posts
