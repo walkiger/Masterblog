1370ba63823f0038c0e49ffd2e1d6053b724bf1f
walkiger <116552530+walkiger@users.noreply.github.com>
2025-01-20 16:54:43 +0100

    updated app.py and index.html initial commit for blog_posts.json

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+import json
 
 app = Flask(__name__)
 
+# Load blog posts from JSON file
+def load_posts():
+    with open('blog_posts.json', 'r') as file:
+        return json.load(file)
+
+# Save blog posts to JSON file
+def save_posts(posts):
+    with open('blog_posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
