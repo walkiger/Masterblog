d57ec50bdceb7216ff80559028e08b6eee085cfa
walkiger <116552530+walkiger@users.noreply.github.com>
2025-01-20 17:01:05 +0100

    added route /add to app.py initial commit for add.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, request, render_template, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -17,6 +17,36 @@ def save_posts(posts):
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # Load existing posts
+        blog_posts = load_posts()
+
+        # Get form data
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        # Create a new blog post with a unique ID
+        new_post = {
+            'id': blog_posts[-1]['id'] + 1 if blog_posts else 1,
+            'author': author,
+            'title': title,
+            'content': content
+        }
+
+        # Add new post to the list
+        blog_posts.append(new_post)
+
+        # Save updated posts to JSON file
+        save_posts(blog_posts)
+
+        # Redirect to the home page
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
